f679897fce58ded51f44562284cb57ada62dc268
Norton Allen <allen@huarp.harvard.edu>
2011-10-19 18:23:48 +0000

    Add write_log

--- TM/HFEMS.cmd
+++ TM/HFEMS.cmd
@@ -3,6 +3,17 @@
   #ifdef SERVER
     #include "address.h"
     #include "subbus.h"
+  
+    void write_savelog( char *s ) {
+      FILE *fp;
+      fp = fopen( "saverun.log", "a" );
+      if ( fp == 0 ) nl_error( 2, "Unable to write to saverun.log" );
+      else {
+	fprintf( fp, "%s\n", s );
+	fclose( fp );
+      }
+    }
+
   #endif
 %}
 
@@ -10,6 +21,7 @@
 %INTERFACE <SoldrvA>
 
 &command
+	: Savelog %s (Enter Log Message) * { write_savelog($2); }
 	: &HFEMScmdset { if_dccc.Turf( "D%d\n", $1 ); }
 	: Set &daspt_cmd %lf (Enter Setpoint Value in Volts) * {
 	    double N = $3 * 6553.6;
