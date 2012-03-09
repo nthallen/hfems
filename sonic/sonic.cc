76040622414787fa63179786c24f4afd27294a0e
Nicholas Demusz <ndemusz@huarp.harvard.edu>
2012-03-09 21:00:34 +0000

    Completely reworked to use new library functions

--- sonic/sonic.cc
+++ sonic/sonic.cc
@@ -1,224 +1,107 @@
-/* Sonic anemometer serial interface
-   Written by NTA 5/9/97
-*/
-#include <termios.h>
-//#include <sys/dev.h>
-//#include <sys/proxy.h>
-//#include <sys/kernel.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <errno.h>
-#include <fcntl.h>
-#include "nortlib.h"
-#include "collect.h"
-#include "oui.h"
-//#include "cltsrvr.h"
 #include "sonic.h"
-#include "Selector.h"
-#include "Timeout.h"
+#include "nortlib.h"
+#include "oui.h"
+
 #define SONIC_REC_SIZE 35
-#include <errno.h>
-#include <string.h>
-#include "tm.h"
 
 
-/** Establish connection to Quit stream */
-Quitter::Quitter() : Selectee() {
-  int resp = set_response(1);
-  fd = tm_open_name(tm_dev_name("cmd/Quit"),NULL,O_RDONLY);
-  flags = Selector::Sel_Read;
-  set_response(resp);
-}
 
-/** If Quitter is ready for read, it's time to quit */
-int Quitter::ProcessData(int flag) {
 
-  return 1;
-}
+const char *sonic_path = "/net/path/to/device";
 
-/*
-void got_quit_proxy( void ) {
-  msg( 0, "Resynchronized %ld times", resynchs );
-  msg( 0, "Untransferred records: %ld", untransferred );
-  msg( 0, "Retransferred records: %ld", retransferred );
-  DONE_MSG;
-  exit(0);
-}
-*/
-
-/** Constructor, sets to NULL so that easier to detect if no connection */
-TMcollect::TMcollect() : Selectee() {
-  par = NULL;
-  TMid = NULL;
-}
-
-int TMcollect::ProcessData( int flag ){
-  flags = 0;
-//  par->Request();
-  return 0;
-}
-
-/** Initialize Col_send */
-int TMcollect::init(sonic_ctrl *par_in, const char *name,
-	    void *data, int data_size){
-  par = par_in;
-  TMid = Col_send_init(name, data, data_size, 0);
-  fd = TMid->fd;
-  flags = Selector::Sel_Write;
-  nl_error( -2, "TM initialized: fd = %d", fd );
-  return 0;
-}
-/** Set collection flag to write */
-int TMcollect::send(){
-  Col_send(TMid);
-  flags = Selector::Sel_Write;
-  return 0;
-}
-
-sonic_ctrl::sonic_ctrl( char* serdevice) : Selectee() {
-  
-  buf = new char[SONIC_REC_SIZE+1];
-  TM.init( this, "Sonic", &TMdata, sizeof(TMdata) );
-  int chars_accepted = 0;
-  int resynchs = 0;
-  int untransferred = 0;
-  int retransferred = 0;
-  sonic_fd = open( serdevice, O_RDONLY );
-  if ( sonic_fd == -1 )
-	nl_error( 3, "Unable to open serial device \"%s\"", serdevice );  
-  collect_proxy = TMcollect::Col_set_proxy( SONIC_PROXY, 0 );
-  quit_proxy    = TMcollect::cc_quit_request( 0 );
-  serdev_proxy  = qnx_proxy_attach( 0, NULL, 0, -1 );
-  if ( serdev_proxy == -1 )	nl_error( 3, "Unable to attach proxy" );
-  if( tcgetattr( fd, &termios_p ) )
-    nl_error( 3, "Invalid fildes" );
-  // following are just place holder values until I figure out what
-  // they should actually be.
-  termios_p.c_cc[VMIN]  =  1; // minimum number of bytes to be read in before a return/ amount of data you'll settle for
-							  // if VMIN set to zero it is PURE timed read. (VMIN = VTIME = 0 non-blocking read)
-  termios_p.c_cc[VTIME] =  0; // measured in 1/10 of a second resets after each char, find out how long typcial delay is.
-  termios_p.c_lflag &= ~( ECHO|ICANON|ISIG|ECHOE|ECHOK|ECHONL );
-  termios_p.c_oflag &= ~( OPOST );
-  if( tcsetattr( fd, TCSADRAIN, &termios_p ) )
-    nl_error( 3, "Argument action is invalid for member of termios_p" );
-}
-
-void sonic_ctrl::sonic_record( short U, short V, short W, short T ) {
-  collect_buf.U = U;
-  collect_buf.V = V;
-  collect_buf.W = W;
-  collect_buf.T = T;
-  if ( collect_buf.transferred )
-	untransferred++;
-  collect_buf.transferred++;
-}
-/*
-void got_collect_proxy( void ) {
-  if ( collect_buf.transferred ) {
-	Col_send(col_id);
-	collect_buf.transferred = 0;
-  } else retransferred++;
-}
-*/
-void sonic_ctrl::sonic_fillbuf( void ) {
-  static int armed = 0;
-  unsigned n;
-/*  
-  while ( armed ) {
-	pid_t who = Receive( 0, NULL, 0 );
-	if ( who == collect_proxy ) got_collect_proxy();
-	else if ( who == quit_proxy ) got_quit_proxy();
-	else if ( who == serdev_proxy ) armed = 0;
-  }
-*/
-  n = ( chars_accepted >= SONIC_REC_SIZE ) ?
-		1 : SONIC_REC_SIZE - chars_accepted;
-  buf_high = dev_read( sonic_fd, buf, SONIC_REC_SIZE, n, 0, 0,
-						serdev_proxy, &armed );
-  if ( buf_high == -1 )
-	nl_error( 2, "Error %d reading from sonic", errno );
-  buf_low = 0;
-}
-
-/** sonic_getc() get a character */
-int sonic_ctrl::sonic_getc( void ) {
-  while ( buf_low >= buf_high ) sonic_fillbuf();
-  chars_accepted++;
-  return buf[buf_low++];
-}
-
-/** returns non-zero if the next character from sonic is not c */
-int sonic_ctrl::sonic_not_c( int c ) {
-  return ( sonic_getc() != c );
-}
-
-/** returns non-zero if input isn't of the form:
-		[ -][0-9][0-9].[0-9][0-9]
-   Assigns the result to *num
-*/
-int sonic_ctrl::sonic_not_num( signed short *num ) {
-  int sign;
-  int c;
-  int n = 0;
-  sign = sonic_getc();
-  if ( sign != ' ' && sign != '-' ) return 1;
-  c = sonic_getc(); if ( ! isdigit(c) ) return 1; n = c - '0';
-  c = sonic_getc(); if ( ! isdigit(c) ) return 1; n = (n*10) + c - '0';
-  if ( sonic_not_c( '.' ) ) return 1;
-  c = sonic_getc(); if ( ! isdigit(c) ) return 1; n = (n*10) + c - '0';
-  c = sonic_getc(); if ( ! isdigit(c) ) return 1; n = (n*10) + c - '0';
-  if ( sign == '-' ) n = -n;
-  *num = n;
-  return 0;
-}
-
-//void sonic_operate( void ) {
-int sonic_ctrl::ProcessData( int flag ){
-//  for (;;) {
-	do { chars_accepted = 0; } while ( sonic_not_c( '\n' ) );
-	resynchs++;
-	for (;;) {
-	  short U, V, W, T;
-
-	  chars_accepted = 0;
-	  if ( sonic_not_c( 'U' ) )  ||
-	  if ( sonic_not_num( &U ) ) ||
-	  if ( sonic_not_c( ' ' ) )  ||
-	  if ( sonic_not_c( ' ' ) )  ||
-	  if ( sonic_not_c( 'V' ) )  ||
-	  if ( sonic_not_num( &V ) ) ||
-	  if ( sonic_not_c( ' ' ) )  ||
-	  if ( sonic_not_c( ' ' ) )  ||
-	  if ( sonic_not_c( 'W' ) )  ||
-	  if ( sonic_not_num( &W ) ) ||
-	  if ( sonic_not_c( ' ' ) )  ||
-	  if ( sonic_not_c( ' ' ) )  ||
-	  if ( sonic_not_c( 'T' ) )  ||
-	  if ( sonic_not_num( &T ) ) ||
-	  if ( sonic_not_c( '\r' ) ) ||
-	  if ( sonic_not_c( '\n' ) ) 
-		break;
-	  sonic_record( U, V, W, T );
-	}
-	return 0;
-}
-
-int main( int argc, char **argv ) {
+int main(int argc, char **argv) {
   oui_init_options(argc, argv);
-  Selector Sr;
-  if ( optind >= argc )
-	nl_error( 3, "Must specify a serial device: optind = %d", optind );
-  Quitter Q;
-  sonic_ctrl SC( argv[optind] );
-  if ( Q.fd >= 0)
-    Sr.add_child(&Q);
-  Sr.add_child(&SC);
-  Sr.add_child(&SC.TM);
-  if ( serdev_proxy == -1 )	nl_error( 3, "Unable to attach proxy" );
-  Sr.event_loop();
-  nl_error( 0, "Resynchronized %ld times", SC.resynchs );
-  nl_error( 0, "Untransferred records: %ld", SC.untransferred );
-  nl_error( 0, "Retransferred records: %ld", SC.retransferred );
-  
+  { Selector S;
+    Sonic_t SCdata;
+    Sonic SC( sonic_path, &SCdata );
+    SC.setup(38400, 8, 'n', 1, 28, 1 ); //need parameters for Sonic setup
+    Cmd_Selectee QC;
+    TM_Selectee TM( "Sonic", &SCdata, sizeof(SCdata) );
+    S.add_child(&SC);
+    S.add_child(&QC);
+    S.add_child(&TM);
+    S.event_loop();
+  }
+  nl_error( 0, "Terminating" );
+}
+
+Sonic::Sonic( const char *path, Sonic_t *data ) :
+    Ser_Sel( path, O_RDWR | O_NONBLOCK, 40 ) {
+  state = Q1;
+  query_pending = 0;
+  TMdata = data;
+  flags |= Selector::gflag(0);
+  //initialize TMdata if necessary
+  TMdata->SC_stat = SC_PRESENT;
+  nl_error( 0, "Sonic fd = %d, flags = %d", fd, flags );
+}
+
+int Sonic::ProcessData(int flag) {
+  if ( flag & Selector::gflag(0) ) {
+    IssueQuery(1);
+  }else {
+    //int addr1, addr2;
+    short U, V, W, T;
+    if ( flag & Selector::Sel_Read ) {
+      // Read and parse data
+	  // U32767  V32767  W32767  T32767\r\n
+      if ( fillbuf() ) return 1;
+      cp = 0;
+	  if( not_found( '\n' ) return 0;
+	  cp = 0;
+      while ( cp < nc ) {
+	  // for not_int() it doesn't require the address of?
+		if( not_str( "U" ) ||
+		not_int( U ) ||
+		not_str( "  V" ) ||
+		not_int( V ) ||
+		not_str( "  W" )
+		not_int( W ) ||
+		not_str( "  T" ) ||
+		not_int( T ) ||
+		not_str( "\r\n" ){
+    	  if ( cp < nc ) {
+	        consume(cp);
+	      }
+	    } else {
+	      TMdata->U = U;
+		  TMdata->V = V;
+		  TMdata->W = W;
+		  TMdata->T = T;
+	      TMdata->SC_stat |= SC_FRESH;
+	      state = Q1;
+	    }
+	    consume(cp);
+	    if (query_pending )
+	      IssueQuery(0);
+	  }
+    }
+  }
   return 0;
+}
+
+void Sonic::IssueQuery(bool synch) {
+  const char *query;
+  query_pending = synch;
+  int rv;
+
+  if ( synch ) {
+    TMdata->SC_stat &= ~(SC_FRESH);
+  }
+  switch (state) {
+    case R1:
+      report_err("Timeout reading from device");
+      // fall through
+    case Q1:
+      query = "replace with what a query looks like?";
+      state = R1;
+      break;
+  }
+  rv = write( fd, query, 11 );
+  if ( rv < 0 ) {
+    report_err( "Error %d writing to Sonic", errno );
+  } else if ( rv < 11 ) {
+    report_err( "Wrote only %d bytes", rv );
+  }
 }
--- sonic/sonic.h
+++ sonic/sonic.h
@@ -1,71 +1,30 @@
-/** Data structure for transfer of data from sonic anemometer 
-  * transferred is not a data value, but is used to indicate that
-  * data was successfully transferred to collection. In
-  * collection, after the data is entered into the frame,
-  * transferred is set to 0. sonic will always set this value to
-  * 1, so when it comes time to fill the frame again, we can see
-  * whether new data has arrived or not
-*/
+#ifndef SONIC_H_INCLUDED
+#define SONIC_H_INCLUDED
 
-#ifndef _SONIC_H_INCLUDED
-
-
-#include "Selector.h"
-#include "collect.h"
-
-#define _SONIC_H_INCLUDED
+#ifdef __cplusplus
+#include "SerSelector.h"
 typedef struct {
   short U, V, W, T;
-  short transferred;
+  unsigned char SC_stat;
 } sonic_t;
-class sonic_ctrl;
-class TMcollect : public Selectee {
+#define SC_PRESENT 1
+#define SC_FRESH 2
+
+enum SC_next { Q1, R1 };
+
+class Sonic : public Ser_Sel {
   public:
-    TMcollect();
+    Sonic( const char *path, Sonic_t *data );
     int ProcessData(int flag);
-    int init(sonic_ctrl *par, const char *name, void *data, int data_size);
-    int send();
   private:
-    send_id TMid;
-	sonic_ctrl *par;
-};
-
-class Quitter : public Selectee {
-  public:
-    Quitter();
-    int ProcessData(int flag);
-};
-
-
-class sonic_ctrl : public Selectee {
-  public:
-    TMcollect TM;
-	struct termios termios_p;
-	sonic_ctrl(char* serdevice);
-	int ProcessData(int flag);
-	void sonic_record( short U, short V, short W, short T );
-	void sonic_fillbuf( void );
-	int sonic_getc( void );
-	int sonic_not_c( int c );
-	int sonic_not_num( signed short *num );
-
-	long resynchs;
-	long untransferred;
-	long retransferred;	
-  private:
-	char *buf;
-	sonic_t TMdata;
-	int sonic_fd;
-	int buf_high, buf_low;
-	int chars_accepted;
-	pid_t collect_proxy;
-	pid_t quit_proxy;
-	pid_t serdev_proxy;
-	send_id col_id;
-	sonic_t collect_buf;
-
+    void IssueQuery(bool synch);
+    SC_next state;
+    bool query_pending;
+    Sonic_t *TMdata;
 };
 
 /* This is a magic number... */
 #define SONIC_PROXY 5
+
+#endif
 #endif
