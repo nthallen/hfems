f314f29c5112b76e4429b7a0a377a1bb84df9302
Nicholas Demusz <ndemusz@huarp.harvard.edu>
2012-03-12 15:44:31 +0000

    compiles

--- sonic/sonic.cc
+++ sonic/sonic.cc
@@ -38,14 +38,14 @@ int Sonic::ProcessData(int flag) {
     IssueQuery(1);
   }else {
     //int addr1, addr2;
-    short U, V, W, T;
+    int U, V, W, T;
     if ( flag & Selector::Sel_Read ) {
       // Read and parse data
 	  // U32767  V32767  W32767  T32767\r\n
       if ( fillbuf() ) return 1;
       cp = 0;
 	  // Do check for a complete record
-	  if( not_found( '\n' ) return 0;
+	  if( not_found( '\n' )) return 0;
 	  cp = 0;
       while ( cp < nc ) {
 	  // for not_int() it doesn't require the address of?
@@ -53,11 +53,11 @@ int Sonic::ProcessData(int flag) {
 		not_int( U ) ||
 		not_str( "  V" ) ||
 		not_int( V ) ||
-		not_str( "  W" )
+		not_str( "  W" ) ||
 		not_int( W ) ||
 		not_str( "  T" ) ||
 		not_int( T ) ||
-		not_str( "\r\n" ){
+		not_str( "\r\n" )){
     	  if ( cp < nc ) {
 	        consume(cp);
 	      }
