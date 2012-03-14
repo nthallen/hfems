c68930ac2bef0f09c93f1faf4c713975b47ec4de
Nicholas Demusz <ndemusz@huarp.harvard.edu>
2012-03-14 14:48:32 +0000

    separating data struct from rest of header file for TM

---
+++ sonic/sonic_int.h
@@ -0,0 +1,25 @@
+#ifndef SONIC_H_INT_INCLUDED
+#define SONIC_H_INT_INCLUDED
+
+#ifdef __cplusplus
+#include "sonic.h"
+#include "SerSelector.h"
+enum SC_next { Q1, R1 };
+
+class Sonic : public Ser_Sel {
+  public:
+    Sonic( const char *path, Sonic_t *data );
+    int ProcessData(int flag);
+  private:
+	int sign_val();
+    void IssueQuery(bool synch);
+    SC_next state;
+    bool query_pending;
+    Sonic_t *TMdata;
+
+};
+
+/* This is a magic number... */
+#define SONIC_PROXY 5
+
+#endif
